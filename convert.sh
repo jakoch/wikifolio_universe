c2b197c89af438f9f310938501090f47f962bee4
Jens A. Koch <jakoch@web.de>
2021-06-21 01:03:34 +0200

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -5,7 +5,7 @@ export SQLITE_FILE="Investment_Universe_$DATE.sqlite"
 
 show_infos() {
   lscpu | egrep 'Model name|Socket|Thread|NUMA|CPU\(s\)'
-  echo "sqlite3 version:"
+  echo -e "sqlite3 version:\n"
   sqlite3 -version
 }
 
