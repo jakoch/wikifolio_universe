52c260bb7ab5bfb54be1519038d976ebdd65b5a9
Jens A. Koch <jakoch@web.de>
2021-09-22 22:58:23 +0200

    new html layout, try to push from data folder to gh-pages

--- convert.sh
+++ convert.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Constants
+
 export DATE=$(TZ=":Europe/Berlin" date +%d_%m_%Y_%H%M)
-export SQLITE_FILE="Investment_Universe_$DATE.sqlite"
-export CSV_FILE="Investment_Universe_$DATE.csv"
+export SQLITE_FILE="Investment_Universe-$DATE.sqlite"
+export CSV_FILE="Investment_Universe-$DATE.csv"
+
+# Helper functions
+
+getFilesize() {
+  ls -sh "$1" | awk '{print $1}';
+}
+
+getSqliteFilename() {
+  ls Investment_Universe-*.sqlite
+}
+
+# ---------------------------------------------------------
 
 show_infos() {
   echo -e "\nCPU Info:"
@@ -12,13 +26,16 @@ show_infos() {
 }
 
 install_wiuc() {
+  echo -e "\nInstalling Wikifolio Universe Converter"
   curl -L --output ./wiuc.zip https://github.com/jakoch/wikifolio_universe_converter/releases/download/v0.1.0/Wikifolio_Investment_Universe_Converter-x64-linux-Clang-12.zip
-  unzip ./wiuc.zip
+  mkdir data
+  unzip ./wiuc.zip -d data
   rm ./wiuc.zip
-  chmod +x wiuc
+  chmod +x data/wiuc
 }
 
 install_7zip() {
+  echo -e "\nInstalling 7zip"
   sudo apt-get install -y p7zip-full
 }
 
@@ -28,27 +45,27 @@ install() {
 }
 
 convert() {
+  cd data
+
   echo -e "\nConvert xlsx to sqlite"
   ./wiuc
+
+  echo -e "\nAdding date and time to filenames"
   mv Investment_Universe.sqlite $SQLITE_FILE
   mv Investment_Universe.csv $CSV_FILE
 
   # show folder
   ls -lash
 
-  echo -e "\nShow database schema"
+  echo -e "\nShowing SQLITE SCHEMA"
   sqlite3 $SQLITE_FILE ".schema"
-}
 
-getFilesize() {
-  ls -sh "$1" | awk '{print $1}';
-}
-
-getSqliteFilename() {
-  ls Investment_Universe_*.sqlite
+  cd ..
 }
 
 compress() {
+  cd data
+
   SQLITE_FILE=$(getSqliteFilename)
 
   # compress sqlite, get filesize
@@ -59,67 +76,98 @@ compress() {
   # show folder
   ls -lash
 
+  # get filesize of xlsx
   EXCEL_FILENAME="Investment_Universe.de.xlsx"
   EXCEL_FILESIZE=$(getFilesize "$EXCEL_FILENAME")
+  rm Investment_Universe.de.xlsx
 
   # write new README
   # ghpages uses jekyll, which has UTF-8 issues. so instead of "Ü" use "&Uuml;"
   echo -n > README.md
   echo -e '# Das gesamte [wikifolio.com Anlageuniversum](https://www.wikifolio.com/de/de/hilfe/tutorials-trader/handel-hinweise/anlageuniversum) im &Uuml;berblick:\n' >> README.md
-  echo -e '\n## Excel\n' >> README.md
-  echo -e '\n- [Investment_Universe.de.xlsx](https://wikifolio.blob.core.windows.net/prod-documents/Investment_Universe.de.xlsx) ('$EXCEL_FILESIZE')\n' >> README.md
-  echo -e '\n## SQLite\n' >> README.md
-  echo -e '\n- ['$SQLITE_ZIP_FILENAME']('$SQLITE_ZIP_FILENAME') ('$SQLITE_ZIP_FILESIZE')\n' >> README.md
+  echo -e '\n #### Downloads vom '$DATE'\n' >> README.md
+  echo -e '| | | |' >> README.md
+  echo -e '|--|--|--|' >> README.md
+  echo -e '| **Investment Universe** | [sqlite]('data/sqlite/$SQLITE_ZIP_FILENAME') ('$SQLITE_ZIP_FILESIZE') | [xlsx](https://wikifolio.blob.core.windows.net/prod-documents/Investment_Universe.de.xlsx) ('$EXCEL_FILESIZE') |' >> README.md
+
+  cd ..
 }
 
-create_tables_for_each_security_type() {
-  SQLITE_FILE=$(getSqliteFilename)
-  sqlite3 $SQLITE_FILE < query/create-tables-for-securitytypes.sql
-}
+create_security_type_databases() {
+  cd data
 
-create_sqlite_for_each_security_type_table() {
   SQLITE_FILE=$(getSqliteFilename)
 
-  echo -e '\n#### By SecurityType\n' >> README.md
+  echo -e '\nCreate tables for each SecurityType in main sqlite file'
+  sqlite3 $SQLITE_FILE < ../query/create-tables-for-securitytypes.sql
 
   echo -e '\nExporting SecurityType tables into sqlite database files'
 
+  echo -e '| *By Security Type* |||' >> README.md
+
   for SECURITYTYPE in $( sqlite3 $SQLITE_FILE "SELECT DISTINCT SecurityType FROM Anlageuniversum" );
   do
-    echo -e '\n-----';
-    echo SecurityType: $SECURITYTYPE;
+    echo -e '\n----- SecurityType: '$SECURITYTYPE'';
+
+    FILENAME=$SECURITYTYPE-$DATE
 
     echo -e 'Exporting data selection to CSV file';
-    sqlite3 -header -csv $SQLITE_FILE "SELECT * FROM $SECURITYTYPE;" > $SECURITYTYPE-$DATE.csv;
+    sqlite3 -header -csv $SQLITE_FILE "SELECT * FROM $SECURITYTYPE;" > $FILENAME.csv;
+
+    echo -e 'Zipping CSV file';
+    declare -A CSV_FILENAME
+    CSV_FILENAME[$SECURITYTYPE]=$FILENAME.csv.zip
+    7z a -mx9 ${CSV_FILENAME[$SECURITYTYPE]} $FILENAME.csv;
+    declare -A CSV_FILESIZE
+    declare CSV_FILESIZE[$SECURITYTYPE]=$(getFilesize ${CSV_FILENAME[$SECURITYTYPE]});
 
     echo -e 'Creating SQLite for CSV file';
-    sqlite3 -csv $SECURITYTYPE-$DATE.sqlite ".import $SECURITYTYPE-$DATE.csv $SECURITYTYPE";
+    sqlite3 -csv $FILENAME.sqlite ".import $FILENAME.csv $SECURITYTYPE";
 
-    echo -e 'Moving CSV file into csv folder for diffing';
-    mkdir -p csv
-    mv $SECURITYTYPE-$DATE.csv csv/
-
-    echo -e 'Create ZIP file';
+    echo -e 'Zipping SQLite file';
     declare -A ZIP_FILENAME
-    ZIP_FILENAME[$SECURITYTYPE]=$SECURITYTYPE-$DATE.sqlite.zip
-    7z a -mx9 ${ZIP_FILENAME[$SECURITYTYPE]} $SECURITYTYPE-$DATE.sqlite;
+    ZIP_FILENAME[$SECURITYTYPE]=$FILENAME.sqlite.zip
+    7z a -mx9 ${ZIP_FILENAME[$SECURITYTYPE]} $FILENAME.sqlite;
     declare -A ZIP_FILESIZE
     declare ZIP_FILESIZE[$SECURITYTYPE]=$(getFilesize ${ZIP_FILENAME[$SECURITYTYPE]});
 
     echo -e 'Append README.md';
-    echo -e '\n- ['${ZIP_FILENAME[$SECURITYTYPE]}']('${ZIP_FILENAME[$SECURITYTYPE]}') ('${ZIP_FILESIZE[$SECURITYTYPE]}')\n' >> README.md;
-    echo -e '\n-----';
+    echo -e '| **'$SECURITYTYPE'** | [sqlite]('data/sqlite/${ZIP_FILENAME[$SECURITYTYPE]}') ('${ZIP_FILESIZE[$SECURITYTYPE]}') | [csv]('data/csv/${CSV_FILENAME[$SECURITYTYPE]}') ('${CSV_FILESIZE[$SECURITYTYPE]}') |' >> README.md;
   done;
+
+  cd ..
+}
+
+move_files() {
+    cd data
+
+    echo -e 'Moving *.csv.zip file into csv folder';
+    mkdir -p csv
+    mv *.csv.zip csv/
+
+    echo -e 'Moving *.sql.zip files into sqlite folder';
+    mkdir -p sqlite
+    mv *.sqlite.zip sqlite/
+
+    echo -e 'Deleting csv and sqlite files';
+    rm *.csv
+    rm *.sqlite
+
+    cd ..
 }
 
 show_files_and_folders() {
-  # show folder: ./
-  ls -lash
-  # show folder: ./csv
-  ls -lash ./csv
+  ls -lash data
+  ls -lash data/csv
+  ls -lash data/sqlite
 }
 
-create_security_type_databases() {
-  create_tables_for_each_security_type
-  create_sqlite_for_each_security_type_table
+run() {
+  show_infos
+  install
+  convert
+  compress
+  create_security_type_databases
+  move_files
+  show_files_and_folders
 }
