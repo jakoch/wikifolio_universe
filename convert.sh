1872ef9ec5e38c9503f9482788ed5c3acca5ee19
Jens A. Koch <jakoch@web.de>
2021-09-22 23:53:30 +0200

    mkdir -p

--- convert.sh
+++ convert.sh
@@ -28,7 +28,7 @@ show_infos() {
 install_wiuc() {
   echo -e "\nInstalling Wikifolio Universe Converter"
   curl -L --output ./wiuc.zip https://github.com/jakoch/wikifolio_universe_converter/releases/download/v0.1.0/Wikifolio_Investment_Universe_Converter-x64-linux-Clang-12.zip
-  mkdir data
+  mkdir -p data
   unzip ./wiuc.zip -d data
   rm ./wiuc.zip
   chmod +x data/wiuc
