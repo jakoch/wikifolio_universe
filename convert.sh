a5af867fa6ec6a7b6822eaeb96ca5c13cb0df5d4
Jens A. Koch <jakoch@web.de>
2023-02-22 13:35:58 +0100

    add retry to curl

--- convert.sh
+++ convert.sh
@@ -29,7 +29,7 @@ install_wiuc() {
   echo -e "\nInstalling Wikifolio Universe Converter"
   version="$(curl -s https://api.github.com/repos/jakoch/wikifolio_universe_converter/releases/latest | jq -r '.tag_name' | cut -c 2-)"; echo "Latest WIUC Version: $version"
   url="https://github.com/jakoch/wikifolio_universe_converter/releases/download/v$version/Wikifolio_Investment_Universe_Converter-v$version-x64-linux-Clang-12.zip"; echo "Download URL: $url"
-  curl -L --output ./wiuc.zip $url
+  curl --retry 3 --retry-all-errors -L --output ./wiuc.zip $url
   mkdir -p data
   unzip ./wiuc.zip -d data
   rm ./wiuc.zip
