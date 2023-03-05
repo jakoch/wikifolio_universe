991af354feda4ef7997e127ae096837d37989335
Jens A. Koch <jakoch@web.de>
2023-03-05 02:33:15 +0100

    updated convert.sh to support diffing (#1)

--- convert.sh
+++ convert.sh
@@ -4,7 +4,9 @@
 # How to run: `. ./convert.sh && show_info`
 #
 
-# Constants
+# === Constants ===
+#
+#
 
 DATE=$(TZ=":Europe/Berlin" date +%d_%m_%Y_%H%M)
 SQLITE_FILE="Investment_Universe-$DATE.sqlite"
@@ -14,8 +16,29 @@ export DATE
 export SQLITE_FILE
 export CSV_FILE
 
-# Helper functions
+# === Helper functions ===
+#
 
+isCISystem() {
+  if [ -n "${CI}" ] && [ "${CI}" != "false" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Returns the size of a file in kilobytes (KB).
+#
+# Arguments:
+#   $1: The name of the file to get the size of. Can include wildcards.
+#
+# Returns:
+#   The size of the file(s) in kilobytes (KB).
+#
+# Example:
+#   To get the size of a file called "my_file.txt":
+#   getFilesize "my_file.txt"
+#
 getFilesize() {
   find . -name "$1" -printf "%kK";
 }
@@ -24,20 +47,86 @@ getSqliteFilename() {
   ls Investment_Universe-*.sqlite
 }
 
-# ---------------------------------------------------------
+# Prints an error message in red color with optional indentation.
+#
+# Arguments:
+#   $1: The error message to print. Defaults to "Mistakes were made."
+#   $2: The indentation level. Defaults to 0 (no indentation).
+#       1 = ident with 2 spaces, 2 indent with 4, 3=6, 4=8 (max.).
+#
+# Returns:
+#   None.
+#
+# Example:
+#   print_error "Error occurred." 1
+#
+print_error() {
+  # Message
+  local error_message=${1:-"Mistakes were made."}
 
-show_infos() {
-  echo -e "\nCPU Info:"
-  lscpu | grep -E 'Model name|Socket|Thread|NUMA|CPU\(s\)'
-  echo -e "\nsqlite3 version:"
-  sqlite3 --version
-  echo -e "\n"
-  csvdiff --version
-  7z | head -2
+  # Indent Levels (0=0, 1=2spaces, 2=4 spaces)
+  local indent_level=${2:-0}
+  local indent_spaces=$((indent_level > 4 ? 8 : indent_level * 2))
+
+  # Red color code
+  local red='\033[0;31m'
+  local nocolor='\033[0m'
+
+  printf "%${indent_spaces}s${red}%s${nocolor}\n" "" "$error_message"
 }
 
+# Prints a status message with optional color and indentation.
+#
+# Usage: print_status "Updating data..." 1 yellow
+#
+# Arguments:
+#   $1: The message to print. Defaults to "Status update."
+#   $2: The indentation level. Defaults to 0 (no indentation).
+#       1 = ident with 2 spaces, 2 indent with 4, 3=6, 4=8 (max.).
+#   $3: The color to use. Defaults to light grey. Possible values are:
+#        "red": indicates an error or failure
+#        "yellow": indicates a warning or continuation
+#        "green": indicates success or completion
+#        "blue": indicates information or explanation
+#        "purple": indicates a special or unusual condition
+#        "cyan": indicates a note or comment
+#
+# Returns:
+#   None.
+print_status() {
+  # Message
+  local status_message=${1:-"Status update."}
+
+  # Indention Levels (0=0, 1=2spaces, 2=4 spaces)
+  local indent_level=${2:-0}
+  local indent_spaces=$((indent_level > 4 ? 8 : indent_level * 2))
+
+  # Color codes, defaults to light grey
+  local color=""
+  case "${3:-}" in
+    "red") color='\033[0;31m';;
+    "yellow") color='\033[0;33m';;
+    "green") color='\033[0;32m';;
+    "blue") color='\033[0;34m';;
+    "purple") color='\033[0;35m';;
+    "cyan") color='\033[0;36m';;
+    *) color='\033[0;37m';; # light grey
+  esac
+  local nocolor='\033[0m'
+
+  # Print the message with the specified color and indentation
+  printf "%${indent_spaces}s${color}%s${nocolor}\n" "" "$status_message"
+}
+
+# ---------------------------------------------------------
+
 install_csvdiff() {
-  echo -e "\nInstalling CSVDiff"
+  print_status "🔽 Installing CSVDiff"
+
+  if [ "$(csvdiff --version)" ]; then
+    print_status "Already installed. Skipping."
+  fi
+
   url="https://github.com/aswinkarthik/csvdiff/releases/download/v1.4.0/csvdiff_1.4.0_linux_64-bit.deb";
   # --retry-all-errors only supported by curl v7.71.0+
   curl --retry 3 -L --output ./csvdiff.deb "$url"
@@ -45,7 +134,12 @@ install_csvdiff() {
 }
 
 install_wiuc() {
-  echo -e "\nInstalling Wikifolio Universe Converter"
+  print_status "🔽 Installing Wikifolio Universe Converter"
+
+  if [ -e /data/wiuc ]; then
+    print_status "Already installed. Skipping."
+  fi
+
   version="$(curl -s https://api.github.com/repos/jakoch/wikifolio_universe_converter/releases/latest | jq -r '.tag_name' | cut -c 2-)"; echo "Latest WIUC Version: $version"
   url="https://github.com/jakoch/wikifolio_universe_converter/releases/download/v$version/Wikifolio_Investment_Universe_Converter-v$version-x64-linux-Clang-12.zip"; echo "Download URL: $url"
   # --retry-all-errors only supported by curl v7.71.0+
@@ -57,7 +151,12 @@ install_wiuc() {
 }
 
 install_7zip() {
-  echo -e "\nInstalling 7zip"
+  print_status "🔽 Installing 7zip"
+
+  if [ "$(7z --help)" ]; then
+    print_status "Already installed. Skipping."
+  fi
+
   sudo apt-get install -y p7zip-full
 }
 
@@ -67,25 +166,53 @@ install() {
   install_wiuc
 }
 
+show_infos() {
+  print_status "CPU Info:"
+  lscpu | grep -E 'Model name|Socket|Thread|NUMA|CPU\(s\)'
+  print_status "sqlite3 version:"
+  sqlite3 --version
+  print_status ""
+  csvdiff --version
+  7z | head -2
+}
+
+prepare_data_folder()
+{
+  cd data || return
+
+  # Delete the old readme
+  rm -rf README.md
+
+  # Delete the old sqlite files, we are currently not processing them
+  rm -rf sqlite/*.sqlite.zip
+
+  # Note: at this point the old "csv/*.csv.zip" are still present
+  # These files are deleted in unzip_csv_files()
+
+  cd ..
+}
+
 convert() {
   cd data || return
 
-  echo -e "\nConvert xlsx to sqlite"
+  print_status "Convert xlsx to sqlite and csv using wiuc"
   ./wiuc
 
-  echo -e "\nAdding date and time to filenames"
+  print_status "Adding date and time to filenames"
   mv Investment_Universe.sqlite "$SQLITE_FILE"
   mv Investment_Universe.csv "$CSV_FILE"
 
   # show folder
   ls -lash
 
-  echo -e "\nShowing SQLITE SCHEMA"
+  print_status "Current SQLITE SCHEMA:"
   sqlite3 "$SQLITE_FILE" ".schema"
 
   cd ..
 }
 
+# compresses the xlsx and sqlite investment universe files
+# creates readme with download links
 compress() {
   cd data || return
 
@@ -117,28 +244,28 @@ compress() {
   cd ..
 }
 
-create_security_type_databases() {
+create_databases() {
   cd data || return
 
   SQLITE_FILE=$(getSqliteFilename)
 
-  echo -e '\nCreate tables for each SecurityType in main sqlite file'
+  print_status 'Create tables for each SecurityType in main sqlite file'
   sqlite3 "$SQLITE_FILE" < ../query/create-tables-for-securitytypes.sql
 
-  echo -e '\nExporting SecurityType tables into sqlite database files'
+  print_status 'Exporting SecurityType tables into sqlite database files'
 
   echo -e '| *By Security Type* ||||' >> README.md
 
   for SECURITYTYPE in $( sqlite3 "$SQLITE_FILE" "SELECT DISTINCT SecurityType FROM Anlageuniversum" );
   do
-    echo -e '\n----- SecurityType: '"$SECURITYTYPE"'';
+    print_status '----- SecurityType: '"$SECURITYTYPE"'';
 
     FILENAME=$SECURITYTYPE-$DATE
 
-    echo -e 'Exporting data selection to CSV file';
+    print_status 'Exporting data selection to CSV file';
     sqlite3 -header -csv "$SQLITE_FILE" "SELECT * FROM $SECURITYTYPE;" > "$FILENAME.csv";
 
-    echo -e 'Zipping CSV file';
+    print_status 'Zipping CSV file';
     declare -A CSV_FILENAME
     CSV_FILENAME[$SECURITYTYPE]=$FILENAME.csv.zip
     7z a -mx9 "${CSV_FILENAME[$SECURITYTYPE]}" "$FILENAME.csv";
@@ -146,10 +273,10 @@ create_security_type_databases() {
     CSV_FILESIZE[$SECURITYTYPE]=$(getFilesize "${CSV_FILENAME[$SECURITYTYPE]}");
     declare CSV_FILESIZE
 
-    echo -e 'Creating SQLite for CSV file';
+    print_status 'Creating SQLite for CSV file';
     sqlite3 -csv "$FILENAME.sqlite" ".import $FILENAME.csv $SECURITYTYPE";
 
-    echo -e 'Zipping SQLite file';
+    print_status 'Zipping SQLite file';
     declare -A ZIP_FILENAME
     ZIP_FILENAME[$SECURITYTYPE]=$FILENAME.sqlite.zip
     7z a -mx9 "${ZIP_FILENAME[$SECURITYTYPE]}" "$FILENAME.sqlite";
@@ -157,51 +284,186 @@ create_security_type_databases() {
     ZIP_FILESIZE[$SECURITYTYPE]=$(getFilesize "${ZIP_FILENAME[$SECURITYTYPE]}");
     declare ZIP_FILESIZE
 
-    echo -e 'Append README.md';
+    print_status 'Append README.md';
     echo -e '| **'"$SECURITYTYPE"'** |  | [sqlite]('sqlite/"${ZIP_FILENAME[$SECURITYTYPE]}"') ('"${ZIP_FILESIZE[$SECURITYTYPE]}"') | [csv]('csv/"${CSV_FILENAME[$SECURITYTYPE]}"') ('"${CSV_FILESIZE[$SECURITYTYPE]}"') |' >> README.md;
   done;
 
   cd ..
 }
 
-move_files() {
-    cd data || return
+unzip_old_csv_files() {
+  cd data || return
 
-    echo -e 'Moving *.csv.zip file into csv folder';
-    mkdir -p csv
-    mv ./*.csv.zip csv/
+  # we do not handle the Derivatives, because of large daily changesets (Daily Options)
+  print_status 'Unzipping CSV files' 0 blue;
+  unzip 'csv/ETF-*.csv.zip' -d 'csv/old'
+  unzip 'csv/Stock-*.csv.zip' -d 'csv/old'
+  unzip 'csv/Wikifolios-*.csv.zip' -d 'csv/old'
 
-    echo -e 'Moving *.sql.zip files into sqlite folder';
-    mkdir -p sqlite
-    mv ./*.sqlite.zip sqlite/
+  # delete all old "csv.zip" files
+  rm -rf csv/*.csv.zip
 
-    echo -e 'Deleting *.csv and *.sqlite files';
-    rm ./*.csv
-    rm ./*.sqlite
-
-    cd ..
+  cd ..
 }
 
-show_files_and_folders() {
+diff_csv_files() {
+  cd data || return
+
+  print_status 'Diffing CSV files' 0 blue
+
+  DIFF_SUMMARY_FILE="Diff-Summary-$DATE.json"
+
+  for SECURITYTYPE in ETF Stock Wikifolios; do # Derivaties excluded
+
+    print_status "Finding CSV files for $SECURITYTYPE" 1
+    OLD_CSV=$(find . -wholename "./csv/old/$SECURITYTYPE-*.csv" -printf '%T@ %p\n' | sort -n | tail -n1 | cut -d' ' -f2-)
+    NEW_CSV=$(find . -wholename "./$SECURITYTYPE-*.csv" -printf '%T@ %p\n' | sort -n | tail -n1 | cut -d' ' -f2-)
+
+    if [ ! -f "$OLD_CSV" ] || [ ! -f "$NEW_CSV" ]; then
+      print_error "Error: File not found. File $OLD_CSV $NEW_CSV does not exist." 1
+      continue
+    fi
+
+    print_status "Diffing CSV files for $SECURITYTYPE ($NEW_CSV <-> $OLD_CSV )" 1
+    DIFF_FILE="$SECURITYTYPE-$DATE-Diff.json"
+    # the diffing order is important: first arg new, second arg old.
+    csvdiff "$NEW_CSV" "$OLD_CSV" -o=json > "$DIFF_FILE"
+    print_status "Created Diff file: $DIFF_FILE" 1 yellow
+
+    # csvdiff returns no content, if files are identical (= no file difference).
+    if ! [ -s "$DIFF_FILE" ]; then
+      # Diff File is empty or does not exist.
+      print_status "No Changes detected." 2 yellow
+      # Create Empty JSON diff file
+      SUMMARY_JSON="{ Additions: [], Modifications: [], Deletions: [] }"
+      echo "$SUMMARY_JSON" > "$DIFF_FILE"
+    fi
+
+    print_status 'Calculate Changes Summary and add to diff file' 1
+    SUMMARY_JSON=$(jq '{ Additions: .Additions | length, Modifications: .Modifications | length, Deletions: .Deletions | length }' "$DIFF_FILE")
+    SUMMARY_JSON_FILE=$(jq --argjson SUMMARY "$SUMMARY_JSON" '.Summary |= $SUMMARY' "$DIFF_FILE")
+    echo "$SUMMARY_JSON_FILE" > "$DIFF_FILE"
+    # apply 2 space idention to the json file
+    FORMATTING=$(jq '.' --indent 2 "$DIFF_FILE")
+    echo "$FORMATTING" > "$DIFF_FILE"
+    #
+    # "jq -c ." = output without any whitespace
+
+    print_status "Add Changes Summary to $DIFF_SUMMARY_FILE" 1
+    if ! [ -f "$DIFF_SUMMARY_FILE" ]; then
+      # Create a new JSON object with the desired key-value pair
+      echo "{\"$SECURITYTYPE\": $SUMMARY_JSON}" >> "$DIFF_SUMMARY_FILE"
+    else
+      # Add the new key-value pair to the existing JSON object
+      SUMMARY_JSON_FILE=$(jq --arg SECURITYTYPE "$SECURITYTYPE" --argjson SUMMARY_JSON "$SUMMARY_JSON" '. += {($SECURITYTYPE): $SUMMARY_JSON}' "$DIFF_SUMMARY_FILE")
+      echo "$SUMMARY_JSON_FILE" > "$DIFF_SUMMARY_FILE"
+    fi
+  done
+
+  print_status "Calculate Overall Summary for $DIFF_SUMMARY_FILE" 1 yellow
+  TOTAL_OUT=$(jq '.Totals = {"Additions": ([.ETF, .Stock, .Wikifolios] | map(.Additions) | add), "Modifications": ([.ETF, .Stock, .Wikifolios] | map(.Modifications) | add), "Deletions": ([.ETF, .Stock, .Wikifolios] | map(.Deletions) | add)}' "$DIFF_SUMMARY_FILE")
+  echo "$TOTAL_OUT" > "$DIFF_SUMMARY_FILE"
+
+  #jq . "$DIFF_SUMMARY_FILE"
+
+  print_status "Done" 0 green
+
+  cd ..
+}
+
+move_files() {
+  cd data || return
+
+  print_status "Moving files" 0 blue;
+
+  print_status 'Moving *.csv.zip file into csv folder';
+  mkdir -p csv
+  mv ./*.csv.zip csv/
+
+  print_status 'Moving *.sql.zip files into sqlite folder';
+  mkdir -p sqlite
+  mv ./*.sqlite.zip sqlite/
+
+  print_status 'Moving *.json files into json folder';
+  mkdir -p json
+  mv ./*.json json/
+
+  cd ..
+}
+
+delete_files() {
+  cd data || return
+
+    print_status 'Deleting *.csv and *.sqlite files' 0 blue;
+    rm ./*.csv
+    rm ./*.sqlite
+    rm -rf /csv/old
+
+    if isCISystem; then
+      print_status 'Deleting wiuc'
+      rm -rf wiuc
+    fi
+
+  cd ..
+}
+
+show_folders() {
+  print_status 'Listing folders' 0 blue
+  ls -lash ./
   ls -lash data
   ls -lash data/csv
   ls -lash data/sqlite
+  ls -lash data/json
 }
 
-unzip_csv() {
-  # we don't unzip and diff the Derivatives
-  echo -e 'Unzipping CSV files';
-  unzip 'csv/ETF-*.csv.zip'
-  unzip 'csv/Stock-*.csv.zip'
-  unzip 'csv/Wikifolios-*.csv.zip'
+# do a git checkout of gh_pages branch into the data folder
+# this step replicates the checkout step in the github workflow
+checkout_ghpages()
+{
+  print_status "Checkout of gh-pages branch into data folder" 0 blue
+
+  print_status "Initializing the repository"
+  git init data
+  cd data || return
+  git remote add origin https://github.com/jakoch/wikifolio_universe
+
+  print_status "Disabling automatic garbage collection"
+  git config --local gc.auto 0
+
+  print_status "Fetching the repository"
+  git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +refs/heads/gh-pages*:refs/remotes/origin/gh-pages* +refs/tags/gh-pages*:refs/tags/gh-pages*
+
+  print_status "Determining the checkout info"
+  git branch --list --remote origin/gh-pages
+
+  print_status "Checking out the ref"
+  git checkout --progress --force -B gh-pages refs/remotes/origin/gh-pages
+
+  git log -1 --format='%H'
+
+  cd ..
 }
 
 run() {
+  if ! isCISystem; then
+    checkout_ghpages
+  fi
+
   install
   show_infos
+
+  prepare_data_folder
+  unzip_old_csv_files
+
   convert
   compress
-  create_security_type_databases
+
+  create_databases
+
+  diff_csv_files
+
   move_files
-  show_files_and_folders
+  delete_files
+
+  show_folders
 }
