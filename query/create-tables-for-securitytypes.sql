d94125f6b4a46003bc5d067568d03ee9feeefff2
Jens A. Koch <jakoch@web.de>
2025-07-05 22:53:58 +0200

    update database schema to align with latest changes

--- query/create-tables-for-securitytypes.sql
+++ query/create-tables-for-securitytypes.sql
@@ -1,4 +1,4 @@
-CREATE TABLE ETF         AS select ISIN, WKN, Bezeichnung, Anlagegruppe1, Anlageuniversum1, Anlagegruppe2, Anlageuniversum2 from Anlageuniversum where SecurityType="ETF";
-CREATE TABLE Stock       AS select ISIN, WKN, Bezeichnung, Anlagegruppe1, Anlageuniversum1, Anlagegruppe2, Anlageuniversum2 from Anlageuniversum where SecurityType="Stock";
-CREATE TABLE Derivatives AS select ISIN, WKN, Bezeichnung, Anlagegruppe1, Anlageuniversum1, Anlagegruppe2, Anlageuniversum2 from Anlageuniversum where SecurityType="Derivatives";
-CREATE TABLE Wikifolios  AS select ISIN, WKN, Bezeichnung, Anlagegruppe1, Anlageuniversum1, Anlagegruppe2, Anlageuniversum2 from Anlageuniversum where SecurityType="Wikifolios";
+CREATE TABLE ETF         AS select ISIN, WKN, Bezeichnung, Anlagegruppe1, Anlageuniversum1, Anlagegruppe2, Anlageuniversum2, Anlagegruppe3, Anlageuniversum3 from Anlageuniversum where SecurityType="ETF";
+CREATE TABLE Stock       AS select ISIN, WKN, Bezeichnung, Anlagegruppe1, Anlageuniversum1, Anlagegruppe2, Anlageuniversum2, Anlagegruppe3, Anlageuniversum3 from Anlageuniversum where SecurityType="Stock";
+CREATE TABLE Derivatives AS select ISIN, WKN, Bezeichnung, Anlagegruppe1, Anlageuniversum1, Anlagegruppe2, Anlageuniversum2, Anlagegruppe3, Anlageuniversum3 from Anlageuniversum where SecurityType="Derivatives";
+CREATE TABLE Wikifolios  AS select ISIN, WKN, Bezeichnung, Anlagegruppe1, Anlageuniversum1, Anlagegruppe2, Anlageuniversum2, Anlagegruppe3, Anlageuniversum3 from Anlageuniversum where SecurityType="Wikifolios";
